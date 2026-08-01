11f5ec1a22f7564d6091d7faf9957672eb35d5aa
vv-248430449 <248430449@qq.com>
2026-08-02 07:44:06 +0800

    feature：单租户，全局默认

--- src/main/java/ai/openai/vv/callbacks/AIConfigs.java
+++ src/main/java/ai/openai/vv/callbacks/AIConfigs.java
@@ -4,12 +4,15 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
 public class AIConfigs {
 
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.defaultSystem("我叫徐庶， 我5岁。 我希望你以我爸爸的身份和我对话，你不再是ChatGpt。")
+        //单租户，全局默认
+        return builder.defaultToolContext(Map.of("tenantId", "t1")).defaultSystem("我叫徐庶， 我5岁。 我希望你以我爸爸的身份和我对话，你不再是ChatGpt。")
                 .build();
     }
 
